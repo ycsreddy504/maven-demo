ca2656349ecf674b830f99a2d508873c9df85f9a
chandrareddy <chandrareddy2mail@gmail.com>
2024-08-31 10:19:06 +0530

    added new maven demo project

---
+++ src/main/java/com/vedait/sample/EvenOrOdd.java
@@ -0,0 +1,17 @@
+package com.vedait.sample;
+
+public class EvenOrOdd {
+	
+	public static void main(String[] args) {
+		
+		for(int i=0; i<=10; i++) {
+			if(i%2==0) {
+				System.out.println("even numbers");
+			}else {
+				System.out.println("Odd numbers");
+			}
+		}
+		
+	}
+
+}
